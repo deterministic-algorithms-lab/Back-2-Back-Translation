b98dd72f0a942b64eaa7ef0cfb614d9e7b2c692f
Rachit Bansal <rachitbansal2500@hmail.com>
2019-12-28 19:53:07 +0530

    Updated Train

---
+++ train.py
@@ -0,0 +1,81 @@
+import torch
+from torch.data.utils import DataLoader
+from dataset import pll_datst, coll, mono_datst
+from preprocessing import df_en, df_de
+from model import xlmb2b
+
+pll_train_ds = pll_datst(df_prllel)
+mono_train_ds_eng = mono_datst(df_en)
+mono_train_ds_de = mono_datst(df_de, lang='de')
+b_sz = 32
+model_ed = xlmb2b()
+model_de = xlmb2b()
+
+pll_train_loader = DataLoader(pll_train_ds,batch_size=b_sz, collate_fn = partial(coll, pll_dat = True))
+mono_train_loader_en = DataLoader(mono_train_ds_en, batch_size=b_sz, collate_fn = partial(coll, pll_dat =False))
+mono_train_loader_de = DataLoader(mono_train_ds_de, batch_size=b_sz, collate_fn = partial(coll, pll_dat =False))
+optimizer_ed = torch.optim.Adam(model_ed.parameters(), lr = 0.01)
+optimizer_de = torch.optim.Adam(model_ed.parameters(), lr = 0.01)
+criterion = torch.nn.MSELoss()
+
+num_epochs = 20
+thresh = 0.5
+losses = [[1000, 1000]]
+for epoch in range(num_epochs) :
+  if(losses[-1][0]>thresh or losses[-1][1]>thresh) :
+    for i, batch in enumerate(pll_train_loader) :
+      yhat_ed = model_ed(batch)
+      yhat_de = modle_de(yhat_ed)
+      loss_pll = criterion(yhat_ed, batch['Y']['content'])
+      loss_b2b = criterion(yhat_de, batch['X']['content'])
+      loss1 = loss_pll + loss_b2b
+
+      optimizer_ed.zero_grad()
+      # optimizer_de.zero_grad()
+      loss_pll.backward()
+      loss_b2b.backward()
+      optimizer_ed.step()
+      # optimizer_de.step()
+
+      yhat_de = model_de(batch)
+      yhat_ed = modle_de(yhat_de)
+      loss_pll = criterion(yhat_de, batch['X']['content'])
+      loss_b2b = criterion(yhat_ed, batch['Y']['content'])
+      loss2 = loss_pll + loss_b2b
+
+      # optimizer_ed.zero_grad()
+      optimizer_de.zero_grad()
+      loss_pll.backward()
+      loss_b2b.backward()
+      # optimizer_ed.step()
+      optimizer_de.step()
+
+      losses.appened([l1,l2])
+
+  else:
+    for i, batch in enumerate(mono_train_loader_en):
+      yhat_ed = model_ed(batch)
+      yhat_de = modle_de(yhat_ed)
+      loss_b2b = criterion(yhat_de, batch['X']['content'])
+      loss1 = loss_b2b
+
+      optimizer_ed.zero_grad()
+      # optimizer_de.zero_grad()
+      loss_b2b.backward()
+      optimizer_ed.step()
+      # optimizer_de.step()
+      losses.append([loss1])
+
+    for i, batch in enumerate(mono_train_loader_de):
+      yhat_de = model_de(batch)
+      yhat_ed = modle_de(yhat_de)
+      loss_b2b = criterion(yhat_ed, batch['Y']['content'])
+      loss2 = loss_b2b
+
+      # optimizer_ed.zero_grad()
+      optimizer_de.zero_grad()
+      loss_b2b.backward()
+      # optimizer_ed.step()
+      optimizer_de.step()
+
+      losses[-1].append(l2)
