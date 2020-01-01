f945d33827ec1c02e2be46a44545865a92d4dc70
Jeevesh8 <creativityinczenyoga@gmail.com>
2020-01-02 00:31:29 +0530

    Some corrections

--- model.py
+++ model.py
@@ -1,6 +1,5 @@
 import numpy as np
 import torch
-from sampler import simpler_sampler
 
 class xlmb2b(torch.nn.Module):
     def __init__(self, dic, d_model=1024, trfrmr_nlayers=4, batch_size=batch_size, pll_dat=True) :
@@ -12,11 +11,11 @@ class xlmb2b(torch.nn.Module):
         self.pll_data = pll_dat
         self.batch_size = batch_size
         self.mx_tr_seq_len =
-        self.end_tok = #index of end_token in simpler_sampler's dic
+        self.end_tok = #Token id of end token
         self.simpler_sampler = simpler_sampler(dic, b_sz = 256, d_model=1024, lang='en')
         self.final_layer = nn.Linear(self.d_model, self.vocab_size)
         self.softmax = nn.Softmax(dim=0)
-        self.dic_tensor = #tensor with i_th token's id at i_Th position
+        self.dic_tensor = #tensor with i_th token's id at i_th position
 
     def get_tgt_mask(self, tr_len) :
         x = np.zeros((tr_len,tr_len))
@@ -24,8 +23,21 @@ class xlmb2b(torch.nn.Module):
         x[upp_indices] = -np.inf
         return torch.tensor(x)
 
-    def apply_final_layer(self, trfrmr_out) :
+    def convert_mask_to_inf(mask):
+        mask[mask==1] = 0
+        mask[mask==0] = -np.inf
+        return mask
+
+    def final_layer(self, trfrmr_out, mask) :
+        mask = self.convert_mask_to_inf(mask)
+        trfrmr_out = trfrmr_out+mask
         return self.softmax(self.final_linear(trfrmr_out).reshape(-1, self.vocab_size)).reshape(self.batch_size,-1,self.vocab_size)
+
+    def apply_final_layer(self, trfrmr_out, mask, it_no=None, samples_to_do) :
+        if it_no is not None :
+            trfrmr_out, mask = trfrmr_out[samples_to_do], mask[samples_to_do]
+            mask[:,it_no] = 1
+        return self.final_layer(trfrmr_out, mask)
 
     def forward(self, dat, already_embed = False) :                             #dat is a dictionary with keys==keyword args of xlm
 
@@ -46,8 +58,8 @@ class xlmb2b(torch.nn.Module):
                                              tgt_key_padding_mask=out['attention_mask'],
                                              memory_key_padding_mask=inp['attention_mask'])
             trfrmr_out = trfrmr_out.transpse(1,2)
-            probs = self.apply_final_layer(trfrmr_out)
-            return probs, tr_embd
+            probs = self.apply_final_layer(trfrmr_out, out['attention_mask'])
+            return probs, tr_embd, trfrmr_out
 
         else :
 
@@ -57,6 +69,7 @@ class xlmb2b(torch.nn.Module):
             mem_key_pad_mask = inp['attention_mask']
             tgt_mask = self.get_tgt_mask(self.max_tr_seq_len)
             tr_embd = torch.zeros((self.batch_size, self.max_tr_seq_len, self.d_model))
+            tr_embd = tr_embd
             not_done_samples = torch.tensor([i for i in range(self.batch_size)])
             it_no = 0                                                           #if nth word of target sequence is being predicted,
             final_out = []                                                      #then iteration number(it_no) == n-1
@@ -64,10 +77,10 @@ class xlmb2b(torch.nn.Module):
 
             while True :
                 trfrmr_out = self.trnsfrmr_dcodr(tgt=tr_embd.transpose(1,2), memory=sr_embd.transpose(1,2), tgt_mask=tgt_mask,
-                                             tgt_key_padding_mask=tgt_key_pad_mask,
-                                             memory_key_padding_mask=mem_key_pad_mask)
+                                                 tgt_key_padding_mask=tgt_key_pad_mask,
+                                                 memory_key_padding_mask=mem_key_pad_mask)
                 trfrmr_out = trfrmr_out.transpse(1,2)
-                trfrmr_out = self.apply_final_layer(trfrmr_out)
+                trfrmr_out = self.apply_final_layer(trfrmr_out, tgt_key_pad_mask, it_no, not_done_samples)
                 output_at_it_no = torch.zeros((self.batch_size,1))
                 output_at_it_no[not_done_samples] = self.dic_tensor[trfrmr_out[not_done_samples,it_no,:].max(2)[1]]
                 final_out.append(output_at_it_no)
@@ -76,7 +89,8 @@ class xlmb2b(torch.nn.Module):
                 if new_done_samples_len!=0 :
                     lengs[~ind] = it_no+1
                     mem_key_pad_mask[~ind] = torch.zeros((new_done_samples_len, inp['lengths'].max()))
-                not_done_samples = not_done_samples[ind]
+                    tgt_key_pad_mask[~ind] = torch.zeros((new_done_samples_len, self.max_tr_seq_len))
+                    not_done_samples = not_done_samples[ind]
                 tgt_key_pad_mask[ind,it_no+1] = torch.ones((ind.shape[0],1))
                 if not_done_samples.shape[0]==0 or it_no==self.max_tr_seq_len-1:
                     return torch.stack(final_out), tr_embd, lengs
