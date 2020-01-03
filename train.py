34e6c783aba34494d220657b1ef7312010360ee7
Rachit Bansal <rachitbansal2500@gmail.com>
2020-01-03 13:09:16 +0530

    Train with Bleu. Whew. Check.

--- train.py
+++ train.py
@@ -3,12 +3,13 @@ import torch.nn as nn
 from torch.utils.data import DataLoader
 from dataset import pll_datst, coll, mono_datst
 from preprocessing import load_data
-# from model import xlmb2b
+from model import xlmb2b
+import tqdm
 
 from nltk.translate.bleu_score import sentence_bleu
 
 data_obj = load_data()
-df_prllel, df_eng, df_de = data_obj.final_data()
+df_prllel, df_en, df_de = data_obj.final_data()
 
 pll_train_ds = pll_datst(df_prllel)
 mono_train_ds_eng = mono_datst(df_en)
@@ -53,41 +54,66 @@ def convert_to_probs(batch_y) :
 
   return out
 
+def swap(batch,sr_embd,tr_embd) :
+      z = batch['X']
+      z1 = batch['Y']
+      batch['X'] = batch['Y']
+      batch['Y'] = z
+      batch['X']['content'] = tr_embd
+      batch['Y']['content'] = sr_embd
+      return batch, z['content'], z1['content'], z, z1
+
+def calculate_bleu(ref, cand, dict_, weights = (0.25, 0.25, 0.25, 0.25)):
+  """
+     ref: (batch_size, seq_len, 1)
+     cand: (batch_size, seq_len, 1)
+  """
+  references = []
+  candidates = []
+
 num_epochs = 20
 thresh = 0.5
 losses = [[1000, 1000]]
-for epoch in range(num_epochs) :
+for epoch in tqdm(range(num_epochs)) :
   if(losses[-1][0]>thresh or losses[-1][1]>thresh) :
     for i, batch in enumerate(pll_train_loader) :
-      probs, tr_embd, trfrmr_out = model_ed(batch)
+      
+      probs, sr_embd, tr_embd, trfrmr_out = model_ed(batch)
       loss_pll = cross_entropy_loss(probs, convert_to_probs(batch['Y']['content']))
-      # loss_pll = criterion(yhat_ed, batch['Y']['content'])
-      # passing something to model_de to get 'yhat_de'
-      loss_b2b = cross_entropy_loss(yhat_de, convert_to_probs(batch['X']['content']))
+      batch, a, b, c, d = swap(bath, sr_embd, tr_embd)
+      probs_, sr_embd_, tr_embd_, trfrmr_out_ = model_de(batch, True)
+      loss_b2b = cross_entropy_loss(probs_, convert_to_probs(a))
       loss1 = loss_pll + loss_b2b
-
       optimizer_ed.zero_grad()
-      # optimizer_de.zero_grad()
-      loss_pll.backward()
-      loss_b2b.backward()
-      optimizer_ed.step()
-      # optimizer_de.step()
-
-      yhat_de = model_de(batch)
-      yhat_ed = modle_de(yhat_de)
-      loss_pll = cross_entropy_loss(yhat_de, convert_to_probs(batch['X']['content']))
-      loss_b2b = cross_entropy_loss(yhat_ed, convert_to_probs(batch['Y']['content']))
-      loss2 = loss_pll + loss_b2b
-
-      # optimizer_ed.zero_grad()
       optimizer_de.zero_grad()
-      loss_pll.backward()
-      loss_b2b.backward()
-      # optimizer_ed.step()
+      loss1.backward()
       optimizer_de.step()
+      optimizer_ed.step()
 
+      if(epochs%20 == 0):
+        print("Forward Model: ", calculate_bleu(b, model_ed(c), dict_vocab))
+      
+      batch['X']['content'] = b
+      batch['Y']['content'] = a
+
+      probs, sr_embd, tr_embd, trfrmr_out = model_de(batch)
+      loss_pll = cross_entropy_loss(probs, convert_to_probs(batch['Y']['content']))
+      batch, a, b, c, d = swap(bath, sr_embd, tr_embd)
+      probs_, sr_embd_, tr_embd_, trfrmr_out_ = model_ed(batch, True)
+      loss_b2b = cross_entropy_loss(probs_, convert_to_probs(a))
+      loss1 = loss_pll + loss_b2b
+      optimizer_ed.zero_grad()
+      optimizer_de.zero_grad()
+      loss1.backward()
+      optimizer_de.step()
+      optimizer_ed.step()
+
+      if(epochs%20 == 0):
+        print("Backward Model: ", calculate_bleu(b, model_de(c)))
+      
       losses.appened([loss1,loss2])
-
+      
+'''
   else:
     for i, batch in enumerate(mono_train_loader_en):
       probs, tr_embd, trfrmr_out = model_ed(batch)
@@ -115,3 +141,4 @@ for epoch in range(num_epochs) :
       optimizer_de.step()
 
       losses[-1].append(l2)
+ '''
