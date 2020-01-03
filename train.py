d1b8827933080bfe3a4da5533b8173dfc32c4d2d
Rachit Bansal <rachitbansal2500@gmail.com>
2020-01-03 13:45:52 +0530

    Added Monolingual Training

--- train.py
+++ train.py
@@ -54,14 +54,21 @@ def convert_to_probs(batch_y) :
 
   return out
 
-def swap(batch,sr_embd,tr_embd) :
-      z = batch['X']
-      z1 = batch['Y']
-      batch['X'] = batch['Y']
-      batch['Y'] = z
-      batch['X']['content'] = tr_embd
-      batch['Y']['content'] = sr_embd
-      return batch, z['content'], z1['content'], z, z1
+def swap(batch,sr_embd,tr_embd, pll = True) :
+  if pll:
+    z = batch['X']
+    z1 = batch['Y']
+    batch['X'] = batch['Y']
+    batch['Y'] = z
+    batch['X']['content'] = tr_embd
+    batch['Y']['content'] = sr_embd
+    return batch, z['content'], z1['content'], z, z1
+
+  else:
+    z = batch['X']
+    batch['X']['content'] = tr_embd
+
+    return batch, z['content'], z
 
 def calculate_bleu(ref, cand, dict_, weights = (0.25, 0.25, 0.25, 0.25)):
   """
@@ -75,70 +82,82 @@ num_epochs = 20
 thresh = 0.5
 losses = [[1000, 1000]]
 for epoch in tqdm(range(num_epochs)) :
-  if(losses[-1][0]>thresh or losses[-1][1]>thresh) :
-    for i, batch in enumerate(pll_train_loader) :
-      
-      probs, sr_embd, tr_embd, trfrmr_out = model_ed(batch)
-      loss_pll = cross_entropy_loss(probs, convert_to_probs(batch['Y']['content']))
-      batch, a, b, c, d = swap(bath, sr_embd, tr_embd)
-      probs_, sr_embd_, tr_embd_, trfrmr_out_ = model_de(batch, True)
-      loss_b2b = cross_entropy_loss(probs_, convert_to_probs(a))
-      loss1 = loss_pll + loss_b2b
-      optimizer_ed.zero_grad()
-      optimizer_de.zero_grad()
-      loss1.backward()
-      optimizer_de.step()
-      optimizer_ed.step()
 
-      if(epochs%20 == 0):
-        print("Forward Model: ", calculate_bleu(b, model_ed(c), dict_vocab))
-      
-      batch['X']['content'] = b
-      batch['Y']['content'] = a
+  model_ed.pll_dat = True
+  model_de.pll_dat = True
 
-      probs, sr_embd, tr_embd, trfrmr_out = model_de(batch)
-      loss_pll = cross_entropy_loss(probs, convert_to_probs(batch['Y']['content']))
-      batch, a, b, c, d = swap(bath, sr_embd, tr_embd)
-      probs_, sr_embd_, tr_embd_, trfrmr_out_ = model_ed(batch, True)
-      loss_b2b = cross_entropy_loss(probs_, convert_to_probs(a))
-      loss1 = loss_pll + loss_b2b
-      optimizer_ed.zero_grad()
-      optimizer_de.zero_grad()
-      loss1.backward()
-      optimizer_de.step()
-      optimizer_ed.step()
+  for i, batch in enumerate(pll_train_loader) :
 
-      if(epochs%20 == 0):
-        print("Backward Model: ", calculate_bleu(b, model_de(c)))
-      
-      losses.appened([loss1,loss2])
-      
-'''
-  else:
+    probs, sr_embd, tr_embd, trfrmr_out = model_ed(batch)
+    loss_pll = cross_entropy_loss(probs, convert_to_probs(batch['Y']['content']))
+    batch, a, b, c, d = swap(batch, sr_embd, tr_embd)
+    probs_, sr_embd_, tr_embd_, trfrmr_out_ = model_de(batch, True)
+    loss_b2b = cross_entropy_loss(probs_, convert_to_probs(a))
+    loss1 = loss_pll + loss_b2b
+    optimizer_ed.zero_grad()
+    optimizer_de.zero_grad()
+    loss1.backward()
+    optimizer_de.step()
+    optimizer_ed.step()
+
+    if(epochs%20 == 0):
+      print("Forward Model: ", calculate_bleu(b, model_ed(c), dict_vocab))
+    
+    batch['X']['content'] = b
+    batch['Y']['content'] = a
+
+    probs, sr_embd, tr_embd, trfrmr_out = model_de(batch)
+    loss_pll = cross_entropy_loss(probs, convert_to_probs(batch['Y']['content']))
+    batch, a, b, c, d = swap(batch, sr_embd, tr_embd)
+    probs_, sr_embd_, tr_embd_, trfrmr_out_ = model_ed(batch, True)
+    loss_b2b = cross_entropy_loss(probs_, convert_to_probs(a))
+    loss1 = loss_pll + loss_b2b
+    optimizer_ed.zero_grad()
+    optimizer_de.zero_grad()
+    loss1.backward()
+    optimizer_de.step()
+    optimizer_ed.step()
+
+    if(epochs%20 == 0):
+      print("Backward Model: ", calculate_bleu(b, model_de(c), dict_vocab))
+    
+    losses.appened([loss1,loss2])
+
+# Training on monolingual data if the above losses are sufficiently low:
+
+  if(losses[-1][0]<thresh or losses[-1][1]<thresh):
+
+    model_ed.pll_dat = False
+    model_de.pll_dat = False
+
     for i, batch in enumerate(mono_train_loader_en):
-      probs, tr_embd, trfrmr_out = model_ed(batch)
-      yhat_de = modle_de(yh)
-      loss_b2b = cross_entropy_loss(yhat_de, covert_to_probs(batch['X']['content']))
+      probs, sr_embd, tr_embd, lengs = model_ed(batch)
+      batch, a, b = swap(batch, sr_embd, tr_embd, False)
+      probs, sr_embd, tr_embd, lengs = model_de(batch, True)
+      loss_b2b = cross_entropy_loss(probs, covert_to_probs(a))
+
       loss1 = loss_b2b
 
       optimizer_ed.zero_grad()
-      # optimizer_de.zero_grad()
+      optimizer_de.zero_grad()
       loss_b2b.backward()
       optimizer_ed.step()
-      # optimizer_de.step()
+      optimizer_de.step()
+
       losses.append([loss1])
 
     for i, batch in enumerate(mono_train_loader_de):
-      yhat_de = model_de(batch)
-      yhat_ed = modle_de(yhat_de)
-      loss_b2b = criterion(yhat_ed, batch['Y']['content'])
+      probs, sr_embd, tr_embd, lengs = model_de(batch)
+      batch, a, b = swap(batch, sr_embd, tr_embd, False)
+      probs, sr_embd, tr_embd, lengs = model_ed(batch, True)
+      loss_b2b = cross_entropy_loss(probs, covert_to_probs(a))
+
       loss2 = loss_b2b
 
-      # optimizer_ed.zero_grad()
+      optimizer_ed.zero_grad()
       optimizer_de.zero_grad()
       loss_b2b.backward()
-      # optimizer_ed.step()
+      optimizer_ed.step()
       optimizer_de.step()
 
-      losses[-1].append(l2)
- '''
+      losses[-1].append(loss2)
