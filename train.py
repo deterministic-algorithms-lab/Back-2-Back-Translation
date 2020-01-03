1f048a3119e783c1bb67ca5391b463b79e2800d9
Jeevesh8 <creativityinczenyoga@gmail.com>
2020-01-03 17:16:52 +0530

    Little corections in bleu & cross_entropy_loss

--- train.py
+++ train.py
@@ -2,11 +2,11 @@ import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader
 from dataset import pll_datst, coll, mono_datst
-from preprocessing import load_data
+from preprocessing import load_data, tokenizer
 from model import xlmb2b
 import tqdm
 
-from nltk.translate.bleu_score import sentence_bleu
+from nltk.translate.bleu_score import corpus_bleu
 
 data_obj = load_data()
 df_prllel, df_en, df_de = data_obj.final_data()
@@ -15,6 +15,8 @@ pll_train_ds = pll_datst(df_prllel)
 mono_train_ds_eng = mono_datst(df_en)
 mono_train_ds_de = mono_datst(df_de, lang='de')
 b_sz = 32
+batch_size = 32
+d_model = 1024
 model_ed = xlmb2b()
 model_de = xlmb2b()
 
@@ -26,14 +28,14 @@ optimizer_de = torch.optim.Adam(model_ed.parameters(), lr = 0.01)
 mseloss = nn.MSELoss()
 cross_entropy_loss = nn.CrossEntropyLoss()
 
-def calculate_bleu(ref, cand, dict_, weights = (0.25, 0.25, 0.25, 0.25)):
+def calculate_bleu(ref, cand, weights = (0.25, 0.25, 0.25, 0.25)):
   """
      ref: (batch_size, seq_len, 1)
      cand: (batch_size, seq_len, 1)
   """
   references = []
   candidates = []
-  
+  dict_ = tokenizer.decoder
   for i in range(ref.shape[0]):
     refs = []
     cands = []
@@ -45,14 +47,13 @@ def calculate_bleu(ref, cand, dict_, weights = (0.25, 0.25, 0.25, 0.25)):
 
   return corpus_bleu(references, candidates, weights)
 
-def convert_to_probs(batch_y) :
-  '''returns indices which should be 1'''
-  out = torch.zeros(batch_y.shape[0], batch_y.shape[1], vocab_size)
-  for i in range(batch_y.shape[0]):
-    for j in range(batch_y[i].shape[0]):
-      out[i][j][batch_y[i][j]] = 1
-
-  return out
+def reshape_n_edit(probs) :
+  '''returns probs while removing rows with all 0 probs
+     the rows with all 0 probs are due to padding of all
+     sequences to same length'''
+  x = torch.zeros((vocab_size))
+  y = probs.reshape(-1,d_model)
+  return y[y!=x]
 
 def swap(batch,sr_embd,tr_embd, pll = True) :
   if pll:
@@ -70,14 +71,6 @@ def swap(batch,sr_embd,tr_embd, pll = True) :
 
     return batch, z['content'], z
 
-def calculate_bleu(ref, cand, dict_, weights = (0.25, 0.25, 0.25, 0.25)):
-  """
-     ref: (batch_size, seq_len, 1)
-     cand: (batch_size, seq_len, 1)
-  """
-  references = []
-  candidates = []
-
 num_epochs = 20
 thresh = 0.5
 losses = [[1000, 1000]]
@@ -92,7 +85,7 @@ for epoch in tqdm(range(num_epochs)) :
     loss_pll = cross_entropy_loss(probs, convert_to_probs(batch['Y']['content']))
     batch, a, b, c, d = swap(batch, sr_embd, tr_embd)
     probs_, sr_embd_, tr_embd_, trfrmr_out_ = model_de(batch, True)
-    loss_b2b = cross_entropy_loss(probs_, convert_to_probs(a))
+    loss_b2b = cross_entropy_loss(reshape_n_edit(probs_), a.reshape(-1,1)) #since token_id is same as position in vocabulary
     loss1 = loss_pll + loss_b2b
     optimizer_ed.zero_grad()
     optimizer_de.zero_grad()
@@ -102,7 +95,7 @@ for epoch in tqdm(range(num_epochs)) :
 
     if(epochs%20 == 0):
       print("Forward Model: ", calculate_bleu(b, model_ed(c), dict_vocab))
-    
+
     batch['X']['content'] = b
     batch['Y']['content'] = a
 
