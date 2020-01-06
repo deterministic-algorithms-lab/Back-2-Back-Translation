5b79aa3acc77ddc919120feb0be690a3ba92d7eb
Rachit Bansal <rachitbansal2500@gmail.com>
2020-01-06 16:13:57 +0530

    With print statements

--- preprocessing.py
+++ preprocessing.py
@@ -1,14 +1,15 @@
 import torch
 import pandas as pd
-from transformers import XLMTokenizer, XLMWithLMHeadModel
-
+from transformers import XLMTokenizer, XLMWithLMHeadModel, XLMModel
+import numpy as np
 tokenizer = XLMTokenizer.from_pretrained("xlm-mlm-ende-1024")
 
 class load_data():
-	def __init__(self, paths = ['../../data/train.en','../../data/train.de'], pll_size = 10**5):
+	def __init__(self, load_ = True, paths = ['../../data/train.en','../../data/train.de'], pll_size = 10**5):
 		self.src_lang_path = paths[0]
 		self.trgt_lang_path = paths[1]
 		self.pll_size = pll_size
+		self.load_ = load_
 
 	def load(self):
 		i = 0
@@ -16,21 +17,32 @@ class load_data():
 		self.trgt_tokens = []
 		with open(self.src_lang_path, 'rt') as f:
 		  while(i!=self.pll_size):
-		    input_ids = torch.tensor(tokenizer.encode('<s>'+f.readline()+'</s>')[1:-1])
+		    input_ids = torch.tensor(tokenizer.encode('<s>'+f.readline()+'</s>')[1:-1]).numpy()
 		    self.src_tokens.append(input_ids)
 		    i = i + 1
 
 		with open(self.trgt_lang_path, 'rt') as f:
 		  while(i!=2*self.pll_size):
-		    input_ids = torch.tensor(tokenizer.encode(f.readline()))
+		    input_ids = torch.tensor(tokenizer.encode('<s>'+f.readline()+'</s>')[1:-1]).numpy()
 		    self.trgt_tokens.append(input_ids)
 		    i = i + 1
 
 	def final_data(self):
-		self.load()
-		zipped_list = list(zip(self.src_tokens, self.trgt_tokens))
-		df_prllel = pd.DataFrame(zipped_list, columns = ['en', 'de'], dtype=object)
-		df_eng = pd.DataFrame(self.src_tokens)
-		df_de = pd.DataFrame(self.trgt_tokens)
+		if(self.load_):
+			self.load()
+			zipped_list = list(zip(self.src_tokens, self.trgt_tokens))
+			df_prllel = pd.DataFrame(zipped_list, columns = ['en', 'de'], dtype=object)
+			df_eng = pd.DataFrame(self.src_tokens)
+			df_de = pd.DataFrame(self.trgt_tokens)
+			d = 0
+
+			for df in [df_prllel, df_eng, df_de]:
+				df.to_csv('../../data/file_'+str(d)+'.csv', header=False, index=False)
+				d = d+1
+		else:
+			df_prllel = pd.read_csv('../../data/file_0.csv',header=None, dtype=np.float64, names=['en','de'])
+			print("Hererere", df_prllel.loc[0,'en'])
+			df_eng = pd.read_csv('../../data/file_1.csv', header=None, dtype=np.float64)
+			df_de = pd.read_csv('../../data/file_2.csv', header=None, dtype=np.float64, names=['en','de'])
 
 		return df_prllel, df_eng, df_de
--- train.py
+++ train.py
@@ -4,17 +4,28 @@ from torch.utils.data import DataLoader
 from dataset import pll_datst, coll, mono_datst
 from preprocessing import load_data, tokenizer
 from model import xlmb2b
-import tqdm
+from tqdm import tqdm
+from os import path
+from functools import partial
 
 from nltk.translate.bleu_score import corpus_bleu
 
-data_obj = load_data()
+print("Dependencies Imported \n")
+
+if path.exists("../../data/file_1.csv"):
+	data_obj = load_data(load_ = False)
+else:
+	data_obj = load_data()
+
 df_prllel, df_en, df_de = data_obj.final_data()
 
 pll_train_ds = pll_datst(df_prllel)
-mono_train_ds_eng = mono_datst(df_en)
+mono_train_ds_en = mono_datst(df_en)
 mono_train_ds_de = mono_datst(df_de, lang='de')
-vocab_size = tokenizer.vocab_size()
+
+print("Data Loading done \n")
+
+vocab_size = tokenizer.vocab_size
 
 b_sz = 32
 batch_size = 32
@@ -22,9 +33,14 @@ d_model = 1024
 model_ed = xlmb2b()
 model_de = xlmb2b()
 
+print("Models initialised \n")
+
 pll_train_loader = DataLoader(pll_train_ds,batch_size=b_sz, collate_fn = partial(coll, pll_dat = True))
 mono_train_loader_en = DataLoader(mono_train_ds_en, batch_size=b_sz, collate_fn = partial(coll, pll_dat =False))
 mono_train_loader_de = DataLoader(mono_train_ds_de, batch_size=b_sz, collate_fn = partial(coll, pll_dat =False))
+
+print("Data Loaders Called \n")
+
 optimizer_ed = torch.optim.Adam(model_ed.parameters(), lr = 0.01)
 optimizer_de = torch.optim.Adam(model_ed.parameters(), lr = 0.01)
 mseloss = nn.MSELoss()
@@ -33,7 +49,7 @@ cross_entropy_loss = nn.CrossEntropyLoss()
 def calculate_bleu(ref, cand, weights = (0.25, 0.25, 0.25, 0.25)):
   """
      ref: (batch_size, seq_len, 1)
-     cand: (batch_size, seq_len, vocab_size)
+     cand: (batch_size, seq_len, 1)
   """
   references = []
   candidates = []
@@ -43,12 +59,12 @@ def calculate_bleu(ref, cand, weights = (0.25, 0.25, 0.25, 0.25)):
     cands = []
     for j in range(ref[i].shape[0]):
       refs.append(dict_[ref[i][j]])
-      cands.append(dict_[torch.max(cand[i][j], 0)[1]]))
+      cands.append(dict_[cand[i][j]])
     references.append([refs])
     candidates.append(cands)
 
   return corpus_bleu(references, candidates, weights)
-  
+
 
 def reshape_n_edit(probs) :
   '''returns probs while removing rows with all 0 probs
@@ -57,107 +73,107 @@ def reshape_n_edit(probs) :
   y = probs.reshape(-1,vocab_size)
   return y[y==y]
 
-def swap(batch,sr_embd,tr_embd, pll = True) :
-  if pll:
-    z = batch['X']
-    z1 = batch['Y']
-    batch['X'] = batch['Y']
-    batch['Y'] = z
-    batch['X']['content'] = tr_embd
-    batch['Y']['content'] = sr_embd
+def swap(batch,sr_embd,tr_embd,pll=True) :
+    if pll:
+        z = batch['X']
+        z1 = batch['Y']
+        batch['X'] = batch['Y']
+        batch['Y'] = z
+        batch['X']['content'] = tr_embd
+        batch['Y']['content'] = sr_embd
 
-    return batch, z['content'], z1['content'], z, z1
+        return batch, z['content'], z1['content'], z, z1
 
-  else:
-    z = batch['X']
-    batch['X']['content'] = tr_embd
+    else:
+        z = batch['X']
+        batch['X']['content'] = tr_embd
 
-    return batch, z['content'], z
+    return batch, z['content'], z, None, None
+
+def freeze_weights(model) :
+    for param in model.parameters() :
+        param.requires_grad = False
+
+def unfreeze_weights(model) :
+    for param in model.parameters() :
+        param.requires_grad = True
+
+
+def set_to_eval(model_lis, beam_size=3) :
+    for model in model_lis :
+        model.eval()
+        model.beam_size = beam_size
+
+def evaluate(model, i, beam_size=3) :
+    set_to_eval(model,beam_size)
+    print(str(i)+"th, Forward Model: ", model[0](c))
+    print(str(i)+"th, Backward Model: ", model[1](d))
+
+def run(model_forward,model_backward,batch,optimizers,pll=True,send_trfrmr_out=False):
+    probs, sr_embd, tr_embd, trfrmr_out = model_forward(batch)
+    if pll : loss_pll = cross_entropy_loss(reshape_n_edit(probs), batch['Y']['content'].reshape(-1,1))
+    if not send_trfrmr_out :
+        batch, a, b, c, d = swap(batch, batch['X']['content'], trfrmr_out, pll)
+    batch, a, b, c, d = swap(batch, sr_embd, tr_embd, pll)
+    probs_, sr_embd_, tr_embd_, trfrmr_out_ = model_backward(batch, not send_trfrmr_out)
+    loss_b2b = cross_entropy_loss(reshape_n_edit(probs_), a.reshape(-1,1)) #since token_id is same as position in vocabulary
+    if pll : loss = loss_pll + loss_b2b
+    else : loss = loss_b2b
+    for optimizer in optimizers :
+        optimizer.zero_grad()
+    loss.backward()
+    for optimizer in optimizers :
+        optimizer.step()
+    return a,b,loss
+
+print("Functions defined \n")
+
 
 num_epochs = 1000
 thresh = 0.5
-losses = [[1000, 1000]]
+# losses_epochs = []
+losses_epochs = {"pll" : [], "mono": []}
+optimizers = [optimizer_de,optimizer_ed]
 
 for epoch in tqdm(range(num_epochs)) :
 
-  model_ed.pll_dat = True
-  model_de.pll_dat = True
+    print(epoch)
+    model_ed.pll_dat=True
+    model_de.pll_dat=True
+    losses = [[], []]
 
-  for i, batch in enumerate(pll_train_loader) :
+    for i, batch in enumerate(pll_train_loader) :
 
-    probs, sr_embd, tr_embd, trfrmr_out = model_ed(batch)
-    loss_pll = cross_entropy_loss(reshape_n_edit(probs), batch['Y']['content'].reshape(-1,1))
-    batch, a, b, c, d = swap(batch, sr_embd, tr_embd)
-    probs_, sr_embd_, tr_embd_, trfrmr_out_ = model_de(batch, True)
-    loss_b2b = cross_entropy_loss(reshape_n_edit(probs_), a.reshape(-1,1)) #since token_id is same as position in vocabulary
-    loss1 = loss_pll + loss_b2b
-    optimizer_ed.zero_grad()
-    optimizer_de.zero_grad()
-    loss1.backward()
-    optimizer_de.step()
-    optimizer_ed.step()
+        batch['Y']['content'], batch['X']['content'], loss1 = run(model_ed,model_de,batch,optimizers)
+        if epoch%20==0 : evaluate([model_ed,model_de], 1, beam_size=3)
+        _,_,loss2 = run(model_de,model_ed,batch,optimizers)
+        if epoch%20==0 : evaluate([model_ed,model_de], 2, beam_size=3)
+        losses[0].append(loss1)
+        losses[1].append(loss2)
+    losses_epochs['pll'].append([losses[0].sum()/len(losses[0]), losses[1].sum()/len(losses[1])])
+#    losses_epochs.append({'pll' : [losses[0].sum()/len(losses[0]), losses[1].sum()/len(losses[1])]})
 
-    if(epochs%20 == 0):
-      print("1st, Forward Model: ", calculate_bleu(b, probs))
-      print("1st, Backward Model: ", calculate_bleu(b, probs_))
+#Training on monolingual data if the above losses are sufficiently low:
 
-    batch['X']['content'] = b
-    batch['Y']['content'] = a
+    if(losses_epochs['pll'][-1][0]<thresh or losses['pll'][-1][1]<thresh):
 
-    probs, sr_embd, tr_embd, trfrmr_out = model_de(batch)
-    loss_pll = cross_entropy_loss(reshape_n_edit(probs), batch['Y']['content'].reshape(-1,1))
-    batch, a, b, c, d = swap(batch, sr_embd, tr_embd)
-    probs_, sr_embd_, tr_embd_, trfrmr_out_ = model_ed(batch, True)
-    loss_b2b = cross_entropy_loss(reshape_n_edit(probs_), a.reshape(-1,1))
-    loss1 = loss_pll + loss_b2b
-    optimizer_ed.zero_grad()
-    optimizer_de.zero_grad()
-    loss1.backward()
-    optimizer_de.step()
-    optimizer_ed.step()
+        print("Going for Monolingual Training")
 
+        model_ed.pll_dat = False
+        model_de.pll_dat = False
+        losses = [[], []]
 
-    if(epochs%20 == 0):
-      print("2nd, Forward Model: ", calculate_bleu(b, probs))
-      print("2nd, Backward Model: ", calculate_bleu(b, probs_))
-    
-    losses.appened([loss1,loss2])
+        for i, batch in enumerate(mono_train_loader_en):
 
-# Training on monolingual data if the above losses are sufficiently low:
+            _,_,loss1 = run(model_ed,model_de,batch,optimizers,pll=False)
 
-  if(losses[-1][0]<thresh or losses[-1][1]<thresh):
+            losses[0].append(loss1)
 
-    model_ed.pll_dat = False
-    model_de.pll_dat = False
+        for i, batch in enumerate(mono_train_loader_de):
 
-    for i, batch in enumerate(mono_train_loader_en):
-      probs, sr_embd, tr_embd, lengs = model_ed(batch)
-      batch, a, b = swap(batch, sr_embd, tr_embd, False)
-      probs, sr_embd, tr_embd, lengs = model_de(batch, True)
-      loss_b2b = cross_entropy_loss(probs, covert_to_probs(a))
+            _,_,loss2 = run(model_de,model_ed,batch,optimizers,pll=False)
 
-      loss1 = loss_b2b
+            losses[1].append(loss2)
 
-      optimizer_ed.zero_grad()
-      optimizer_de.zero_grad()
-      loss_b2b.backward()
-      optimizer_ed.step()
-      optimizer_de.step()
-
-      losses.append([loss1])
-
-    for i, batch in enumerate(mono_train_loader_de):
-      probs, sr_embd, tr_embd, lengs = model_de(batch)
-      batch, a, b = swap(batch, sr_embd, tr_embd, False)
-      probs, sr_embd, tr_embd, lengs = model_ed(batch, True)
-      loss_b2b = cross_entropy_loss(probs, covert_to_probs(a))
-
-      loss2 = loss_b2b
-
-      optimizer_ed.zero_grad()
-      optimizer_de.zero_grad()
-      loss_b2b.backward()
-      optimizer_ed.step()
-      optimizer_de.step()
-
-      losses[-1].append(loss2)
+        losses_epochs['mono'].append([losses[0].sum()/len(losses[0]), losses[1].sum()/len(losses[1])])    
+        # losses_epochs.append({'mono':[sum(losses[0])/len(losses[0]), sum(losses[1])/len(losses[1])]})
