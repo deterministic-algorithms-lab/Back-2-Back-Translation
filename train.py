08af745b054d4c9e4ab063fad1abea20c7592580
Rachit Bansal <rachitbansal2500@gmail.com>
2020-02-10 02:41:27 +0530

    imported torch_xla modules

--- train.py
+++ train.py
@@ -53,6 +53,13 @@ import multiprocessing as mp
 from Globals import *
 import argparse
 
+import torch_xla
+import torch_xla.core.xla_model as xm
+import torch_xla.debug.metrics as met
+import torch_xla.distributed.parallel_loader as pl
+import torch_xla.distributed.xla_multiprocessing as xmp
+import torch_xla.utils.utils as xu
+
 parser = argparse.ArgumentParser(description= 'Train the Model')
 parser.add_argument('--dataset_path')
 parser.add_argument('--p', type=float)
